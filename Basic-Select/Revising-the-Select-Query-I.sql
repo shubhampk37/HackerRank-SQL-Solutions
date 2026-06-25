ce21be1c562966f05a01a023f72a879f3c7b63a0
Shubham Kothari <61788363+shubhampk37@users.noreply.github.com>
2026-06-25 14:25:57 +0530

    Update Revising-the-Select-Query-I.sql

--- Basic-Select/Revising-the-Select-Query-I.sql
+++ Basic-Select/Revising-the-Select-Query-I.sql
@@ -1,11 +1,9 @@
 /*
 Problem Name: Revising the Select Query I
-https://www.hackerrank.com/challenges/revising-the-select-query/problem
+Link: https://www.hackerrank.com/challenges/revising-the-select-query/problem
 
 Problem Statement:
 Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA.
-
-The CITY table is described as follows:
 */
 
 --Solution:
